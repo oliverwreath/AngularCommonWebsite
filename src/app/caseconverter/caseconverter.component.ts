fc9e03c92cbca4203b6c1a80b6df81e8dd896d4c
oliverwreath <skywalkerhunter@gmail.com>
2022-09-20 22:53:48 -0400

    AMWR-7 CaseConverter + <> flip + ^V flip

--- src/app/caseconverter/caseconverter.component.ts
+++ src/app/caseconverter/caseconverter.component.ts
@@ -6,13 +6,14 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./caseconverter.component.css']
 })
 export class CaseconverterComponent implements OnInit {
-  oldText: string = 'Example text : Simply enter your, text and choose. the case you want to convert it to. ';
-  private static example: string = 'Example text : Simply enter your, text and choose. the case you want to convert it to. ';
+  oldText: string = '';
+  private static example: string = 'Example text : Simply enter your, text and choose. the case you want to convert it to. \n 2nd row \n 3rd row';
   private static stopWords: string[] = ["and", "to", "the"];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.oldText = String(CaseconverterComponent.example.slice());
   }
 
   onUpper() {
@@ -22,40 +23,61 @@ export class CaseconverterComponent implements OnInit {
   }
 
   onLower() {
+    console.log(this.oldText)
     if (this.oldText) {
       this.oldText = this.oldText.toLowerCase();
     }
   }
 
   onCapitalize() {
-    if (this.oldText) {
-      let words = this.oldText.trim().split(/\s+/)
-      for (let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+    if (!this.oldText)
+      return;
+
+    let rows = this.oldText.split('\n');
+    for (let r = 0; r < rows.length; r++) {
+      if (!rows[r])
+        continue;
+
+      let words = rows[r].split(/\s+/)
+      for (let w = 0; w < words.length; w++) {
+        if (!words[w])
+          continue;
+
+        words[w] = words[w][0].toUpperCase() + words[w].slice(1).toLowerCase();
       }
-      this.oldText = words.join(" ");
+      rows[r] = words.join(" ");
     }
+    this.oldText = rows.join('\n');
   }
 
   onTitleCase() {
-    if (this.oldText) {
-      let s = this.oldText.trim();
+    if (!this.oldText)
+      return;
+
+    let rows = this.oldText.split('\n');
+    for (let r = 0; r < rows.length; r++) {
+      if (!rows[r])
+        continue;
+
+      let s = rows[r];
       const isEndingDot =  (s.endsWith('.'));
       if (isEndingDot)
         s = s.slice(0, s.length - 1);
       let words = s.toLowerCase().split(/\s+/)
-      for (let i = 0; i < words.length; i++) {
-        if (!CaseconverterComponent.stopWords.includes(words[i])) {
-          console.log('1 ' + words[i])
-          words[i] = words[i][0].toUpperCase() + words[i].slice(1).toLowerCase();
+      for (let w = 0; w < words.length; w++) {
+        if (!words[w])
+          continue;
+
+        if (!CaseconverterComponent.stopWords.includes(words[w])) {
+          words[w] = words[w][0].toUpperCase() + words[w].slice(1).toLowerCase();
         } else {
-          console.log('2 ' + words[i])
-          words[i] = words[i].toLowerCase();
+          words[w] = words[w].toLowerCase();
         }
       }
       s = words.join(" ") + (isEndingDot?".":" ");
-      this.oldText = s;
+      rows[r] = s;
     }
+    this.oldText = rows.join('\n');
   }
 
   onSentenceCase() {
@@ -70,5 +92,51 @@ export class CaseconverterComponent implements OnInit {
 
   onExample() {
     this.oldText = CaseconverterComponent.example;
+  }
+
+  onReverse() {
+    if (!this.oldText)
+      return;
+
+    let rows = this.oldText.split('\n');
+    for (let r = 0; r < rows.length; r++) {
+      let row = Array.from(rows[r]);
+      let left = 0, right = row.length - 1;
+      while (left < right) {
+        const tmp = row[left];
+        row[left++] = row[right];
+        row[right--] = tmp;
+      }
+      rows[r] = row.join("");
+    }
+    this.oldText = rows.join('\n');
+  }
+
+  onUpsideDown() {
+    if (!this.oldText)
+      return;
+
+    let rows = this.oldText.split('\n');
+    let left = 0, right = rows.length - 1;
+    while (left < right) {
+      const tmp = rows[left];
+      rows[left++] = rows[right];
+      rows[right--] = tmp;
+    }
+    this.oldText = rows.join('\n');
+  }
+
+  onTrim() {
+    if (!this.oldText)
+      return;
+
+    let rows = this.oldText.split('\n');
+    for (let r = 0; r < rows.length; r++) {
+      if (!rows[r])
+        continue;
+
+      rows[r] = rows[r].trim();
+    }
+    this.oldText = rows.join('\n');
   }
 }
