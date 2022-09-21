4600925d0f39b60fd728d544c832bb4042e55e88
oliverwreath <skywalkerhunter@gmail.com>
2022-09-20 21:10:53 -0400

    AMWR-6 Sticky footer is broken.

--- src/app/diffchecker/diffchecker.component.ts
+++ src/app/diffchecker/diffchecker.component.ts
@@ -8,8 +8,59 @@ import {FormControl, FormGroup} from '@angular/forms';
 })
 export class DiffcheckerComponent implements OnInit {
   form = new FormGroup({
-    oldText: new FormControl<string>(''),
-    newText: new FormControl<string>('')
+    oldText: new FormControl<string>('This part of the\n' +
+      'document has stayed the\n' +
+      'same from version to\n' +
+      'version.  It shouldn\'t\n' +
+      'be shown if it doesn\'t\n' +
+      'change.  Otherwise, that\n' +
+      'would not be helping to\n' +
+      'compress the size of the\n' +
+      'changes.\n' +
+      '\n' +
+      'This paragraph contains\n' +
+      'text that is outdated.\n' +
+      'It will be deleted in the\n' +
+      'near future.\n' +
+      '\n' +
+      'It is important to spell\n' +
+      'check this dokument. On\n' +
+      'the other hand, a\n' +
+      'misspelled word isn\'t\n' +
+      'the end of the world.\n' +
+      'Nothing in the rest of\n' +
+      'this paragraph needs to\n' +
+      'be changed. Things can\n' +
+      'be added after it.'),
+    newText: new FormControl<string>('This is an important\n' +
+      'notice! It should\n' +
+      'therefore be located at\n' +
+      'the beginning of this\n' +
+      'document!\n' +
+      '\n' +
+      'This part of the\n' +
+      'document has stayed the\n' +
+      'same from version to\n' +
+      'version.  It shouldn\'t\n' +
+      'be shown if it doesn\'t\n' +
+      'change.  Otherwise, that\n' +
+      'would not be helping to\n' +
+      'compress the size of the\n' +
+      'changes.\n' +
+      '\n' +
+      'It is important to spell\n' +
+      'check this document. On\n' +
+      'the other hand, a\n' +
+      'misspelled word isn\'t\n' +
+      'the end of the world.\n' +
+      'Nothing in the rest of\n' +
+      'this paragraph needs to\n' +
+      'be changed. Things can\n' +
+      'be added after it.\n' +
+      '\n' +
+      'This paragraph contains\n' +
+      'important new additions\n' +
+      'to this document.')
   })
   oldTextContents: string = '';
   newTextContents: string = '';
