917703d15125fa6189821dced3d9828bfc75cac8
oliverwreath <oliverwreath@users.noreply.github.com>
2022-10-03 22:19:39 -0400

    ANG-24 Csv validate 要print header len，要generate ？？？ (#44)
    ANG-24 Csv validate 要print header len，要generate ？？？

--- src/app/string-utils.service.ts
+++ src/app/string-utils.service.ts
@@ -65,64 +65,8 @@ export class StringUtilsService {
     }
   }
 
-  static validateCsvFile(s: string | null | undefined, separator: Separator, isFirstColumnNotEmpty: boolean): string {
-    let error: string = '';
-
-    if (s) {
-      const rows = s.split(LineBreak.Default);
-      error += StringUtilsService.validateCsvRow(rows[0], 0, separator, isFirstColumnNotEmpty);
-      const len: number = rows[0].split(separator).length;
-      for (let r = 1; r < rows.length; r++) {
-        error += StringUtilsService.validateCsvRowAndLen(rows[r], r, separator, isFirstColumnNotEmpty, len);
-      }
-    }
-    if (error === '') {
-      return 'File is Valid';
-    } else {
-      return error;
-    }
-  }
-
-  static validateCsvRow(row: string, r: number, separator: Separator, isFirstColumnNotEmpty: boolean): string {
-    const fields = row.split(separator);
-    if (isFirstColumnNotEmpty && !fields[0]) {
-      return `Record #${r} has error: 1st column should NOT be empty! ${CsvConverterService.LINE_BREAK}`;
-    } else {
-      return '';
-    }
-  }
-
-  static validateCsvRowAndLen(row: string, r: number, separator: Separator, isFirstColumnNotEmpty: boolean, len: number): string {
-    let error = StringUtilsService.validateCsvRow(row, r, separator, isFirstColumnNotEmpty);
-    const fields = row.split(separator);
-    if (len != fields.length) {
-      const id: string = (fields.length > 0)?fields[0]:'_';
-      error += `Record #${r} (id:${id}) has error: wrong number of fields! Found ${fields.length}, should be ${len}!${CsvConverterService.LINE_BREAK}`;
-    }
-    return error;
-  }
-
   static getRows(text: string): string[] {
     return text.replace(CsvConverterService.LINE_BREAK_REGEX,"\n").split(CsvConverterService.LINE_BREAK);
-  }
-
-  // TODO: move into a ng generate service csvUtil
-  static countUniqueNthColumn(inputText: string, n: number, separator: Separator): string {
-    if (!inputText)
-      return '';
-
-    const rows = StringUtilsService.getRows(inputText);
-    const map = new Map<string, number>();
-    for (let r = 1; r < rows.length; r++) {
-      const row: string = rows[r];
-      const fields: string[] = row.split(separator);
-      const key: string = fields[n - 1];
-      const val = map?.get(key);
-      map.set(key, (val?val:0) + 1);
-    }
-    console.log(map);
-    console.log(JSON.stringify(map));
-    return JSON.stringify(map, (_key, value) => (value instanceof Map ? [...value] : value));
   }
 
   static SELECT: string = 'SELECT';
