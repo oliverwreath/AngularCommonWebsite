9b1ca036c6b97e0b7cb131a9b645e5de4ac313c7
Oli <skywalkerhunter@gmail.com>
2024-07-03 01:47:31 -0400

    Auto-generated commit [ci skip]

--- src/app/caseconverter/characterCount.ts
+++
@@ -1,7 +0,0 @@
-export type CharacterCount = {
-  CHARACTERS: number,
-  WORDS: number,
-  SENTENCES: number,
-  PARAGRAPHS: number,
-  SPACES: number
-};
--- src/app/hero/users.ts
+++
@@ -1,3 +0,0 @@
-export interface DataResponse {
-  data: any;
-}
